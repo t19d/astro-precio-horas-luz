8e306d771a754a2f167e1bf56f3ee5dfb9927c1a
david_tojo <david_tojo@hotmail.es>
2024-05-30 08:12:18 +0200

    feat: :sparkles: Add list to show prices

---
+++ src/components/react/ListPrices/ListPrices.tsx
@@ -0,0 +1,40 @@
+import type { Value } from "@/models/api.model";
+import { experimentalStyled as styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+interface ListPricesProps {
+	prices: Value[];
+}
+
+const Item = styled(Paper)(({ theme }) => ({
+	backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+	...theme.typography.body2,
+	padding: theme.spacing(2),
+	textAlign: "center",
+	color: theme.palette.text.secondary,
+}));
+
+export default function ListPrices({ prices }: ListPricesProps) {
+	// Grid MUI
+
+	return (
+		<Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+			{prices.map((price: Value, index: number) => (
+				<Grid item xs={4} sm={4} md={4} key={index}>
+					<Item>{price.value}</Item>
+				</Grid>
+			))}
+		</Grid>
+	);
+}
+
+// export default function ListPrices({ prices }: ListPricesProps) {
+// 	return (
+// 		<div>
+// 			{prices.map((price: Value, index: number) => (
+// 				<div key={index}>{price.value}</div>
+// 			))}
+// 		</div>
+// 	);
+// }
